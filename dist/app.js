6bb3299556462b03a734197a138ca0e93b9f1da5
YurGoV <yurgov@gmail.com>
2023-03-12 12:56:26 +0200

    curr

---
+++ dist/app.js
@@ -0,0 +1,7 @@
+"use strict";
+const input1 = document.getElementById("num1");
+const input2 = document.getElementById("num2");
+function add(num1, num2) {
+    return num1 + num2;
+}
+//# sourceMappingURL=app.js.map
